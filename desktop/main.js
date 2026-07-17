642b9768d11a83792fd297f3e7a5568b7072a7ac
trinhconghau22111999-arch <trinhconghau22111999@gmail.com>
2026-07-17 17:40:39 +0700

    Implement Telex input method and QR code scanning

--- desktop/main.js
+++ desktop/main.js
@@ -1,9 +1,11 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1280,
     height: 820,
+    icon: path.join(__dirname, 'build', 'icon.ico'),
     webPreferences: {
       webviewTag: true,
       contextIsolation: true,
